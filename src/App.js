14966688314baa570e9e743040ecc46667a34476
Christina Giddens <giddens.christina@gmail.com>
2020-03-03 16:27:54 -0500

    partially complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Photo from "./components/Photo_Section.js";
+
+
 
 function App() {
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=PsxsZpRQ0Zm5AM7FWhmZE399ZYV167gDs7dRyXuR&date=2020-03-01').then(response => {
+          console.log(response);
+          setImage(response.data);
+      });
+  }, []);
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Photo title = {image.title} url = {image.url} date = {image.date} explanation = {image.explanation} />
     </div>
   );
 }
---
+++ src/components/Photo_Section.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Photo(props) {
+    
+
+    return(
+        <div>
+            <h2>{props.title}</h2>
+            <image src = {props.url} />
+            <p>{props.date}</p>
+            <p>{props.explanation}</p>
+        </div>
+    )
+
+}
+
+export default Photo;
