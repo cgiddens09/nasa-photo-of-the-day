42d786013d19522e73bcbf1a5d673dce5f9f0daf
Christina Giddens <giddens.christina@gmail.com>
2020-03-03 21:55:41 -0500

    completed MVP and stretch

--- src/App.js
+++ src/App.js
@@ -7,19 +7,31 @@ import Photo from "./components/Photo_Section.js";
 
 function App() {
   const [image, setImage] = useState([]);
+  const [date, setDate] = useState('2020-03-01');
+
+  const fetchPic = () => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=PsxsZpRQ0Zm5AM7FWhmZE399ZYV167gDs7dRyXuR&date=${date}`).then(response => {
+      console.log(response);
+      setImage(response.data);
+  });
+  }
 
   useEffect(() => {
-      axios.get('https://api.nasa.gov/planetary/apod?api_key=PsxsZpRQ0Zm5AM7FWhmZE399ZYV167gDs7dRyXuR&date=2020-03-01').then(response => {
-          console.log(response);
-          setImage(response.data);
-      });
+      fetchPic(date);
   }, []);
 
+//   useEffect(() => {
+//     axios.get(`https://api.nasa.gov/planetary/apod?api_key=PsxsZpRQ0Zm5AM7FWhmZE399ZYV167gDs7dRyXuR&date=${date}`).then(response => {
+//         console.log(response);
+//         setImage(response.data);
+//     });
+// }, [date]);
 
   return (
     <div className="App">
-      
-      <Photo title = {image.title} url = {image.url} date = {image.date} explanation = {image.explanation} />
+      <input type="date" value={date} onChange={e => setDate(e.target.value)}/>
+      <button onClick={() => fetchPic(date)}>Go!</button>
+      <Photo title={image.title} url={image.url} date={image.date} explanation={image.explanation} />
     </div>
   );
 }
