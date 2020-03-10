e9d59b18d91656f0635ebf748355f892c830f10c
Christina Giddens <giddens.christina@gmail.com>
2020-03-09 21:08:00 -0400

    fixed background

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Photo from "./components/Photo_Section.js";
+import styled from "styled-components";
+
+
+
+const Button = styled.button`
+  width: 10%;
+  background-color: white;
+  color: purple:
+  border: 5px solid lavender;
+  margin-bottom: 3%;
+`;
+
+const Background = styled.div`
+background-image:url('https://cdn2.vectorstock.com/i/1000x1000/68/11/seamless-pattern-with-cat-in-space-the-moon-and-vector-10166811.jpg');
+background-position:cover;
+`;
 
 
 
 function App() {
   const [image, setImage] = useState([]);
-  const [date, setDate] = useState('2020-03-01');
+  const [date, setDate] = useState('2020-03-05');
 
   const fetchPic = () => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=PsxsZpRQ0Zm5AM7FWhmZE399ZYV167gDs7dRyXuR&date=${date}`).then(response => {
@@ -30,8 +46,10 @@ function App() {
   return (
     <div className="App">
       <input type="date" value={date} onChange={e => setDate(e.target.value)}/>
-      <button onClick={() => fetchPic(date)}>Go!</button>
-      <Photo title={image.title} url={image.url} date={image.date} explanation={image.explanation} />
+        <Background>
+          <Button onClick={() => fetchPic(date)}>Go!</Button>
+          <Photo title={image.title} url={image.url} date={image.date} explanation={image.explanation} />
+        </Background>
     </div>
   );
 }
--- src/components/Photo_Section.js
+++ src/components/Photo_Section.js
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import styled from "styled-components";
+
+import  "../App.css";
+import App from "../App";
+
+
+
+const StyledDiv = styled.div`
+    background: #adffd8;
+    color: blue;
+    width: 50%;
+    margin-left: 25%;
+    padding: 2%;
+    img{
+        width:50%;
+    }
+    h1{
+        font-family: 'michroma', sans-serif;
+    }
+`
 
 function Photo(props) {
-    
 
     return(
-        <div>
-            <h2>{props.title}</h2>
-            <img src = {props.url} />
-            <p>{props.date}</p>
-            <p>{props.explanation}</p>
-        </div>
+        
+            <StyledDiv>
+                <h1>Astronomy Photo of the Day</h1>
+                <h2>{props.title}</h2>
+                <img src = {props.url} />
+                <p>{props.date}</p>
+                <p>{props.explanation}</p>
+            </StyledDiv>
+        
     )
 
 }
